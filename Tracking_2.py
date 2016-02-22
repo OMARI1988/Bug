c6d676ac97be4e137038ce992bfb76a897776795
OMARI1988 <omari.1988@gmail.com>
2016-02-22 22:51:56 +0000

    GUI part 1

---
+++ Tracking_2.py
@@ -0,0 +1,170 @@
+import numpy as np
+import cv2
+import colorsys
+import Tkinter as tk
+import Image, ImageTk
+from PIL import Image, ImageTk
+
+
+class Tracker(object):
+    """docstring for Tracker"""
+    def __init__(self):
+        self._Track_flag = 1
+        self._Stop_frame = 10
+        self._Track_number = 0
+        self._Max_Track_number = 8
+        self._HSV_tuples = [(x*1.0/self._Max_Track_number, 1.0, 1.0) for x in range(self._Max_Track_number)]
+        self._colors = map(lambda x: colorsys.hsv_to_rgb(*x), self._HSV_tuples)
+
+        self._cap = cv2.VideoCapture('/home/omari/Bugs/movies/CS3_spotted_wings.avi')
+        self._frames = []
+        while(1):
+            ret, frame = self._cap.read()
+            if ret:
+                self._frames.append(frame)
+            else:
+                break
+        self._N = 0
+        self._N_max = len(self._frames)
+
+
+        self._root = tk.Tk()
+        self._w = tk.Scale(self._root, from_=0, to=self._N_max, length=1000, orient=tk.HORIZONTAL)
+        self._w.pack()
+        self._root.bind('<Escape>', lambda e: self._root.quit())
+        self._lmain = tk.Label(self._root)
+        self._lmain.bind("<Button-1>", self._callback)
+        self._lmain.pack()
+
+
+    def _callback(self,event):
+        print "clicked at", event.x, event.y
+
+    def _show_frame(self):
+        self._frame = self._frames[self._N]
+        self._N += 1
+        # frame = cv2.flip(frame, 1)
+        self._image = cv2.cvtColor(self._frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(self._image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self._lmain.imgtk = imgtk
+        self._lmain.configure(image=imgtk)
+        # self._lmain.after(10, self._show_frame)
+
+
+
+def main():
+    Tr = Tracker()
+    Tr._show_frame()
+    Tr._root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+def get_track(event,x,y,flags,param):
+    global Track_flag,Track_number,Max_Track_number,Stop_frame
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        Track[Track_number] = [[x,y]]
+        Min[Track_number] = [np.inf,[]]
+        Time[Track_number] = [Stop_frame-1]
+        Track_number += 1
+        if Track_number == Max_Track_number:
+            Track_flag = 0
+
+cap = cv2.VideoCapture('/home/omari/Bugs/movies/CS3_spotted_wings.avi')
+fgbg1 = cv2.BackgroundSubtractorMOG(100,10,.03,0)
+fgbg2 = cv2.BackgroundSubtractorMOG2(0,150,False)
+
+cv2.namedWindow('video')
+cv2.setMouseCallback('video',get_track)
+
+frames = []
+Track = {}
+Time = {}
+Min = {}
+while(1):
+    ret, frame = cap.read()
+    if ret:
+        frames.append(frame)
+    else:
+        break
+
+for count,frame in enumerate(frames):
+    print 'processing frame number : ',count+1
+    if count==Stop_frame:
+        while(Track_flag):
+            k = cv2.waitKey(30) & 0xff
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    th = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,5)
+    image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+
+    fgmask = fgbg2.apply(image)
+
+    contours, t = cv2.findContours(fgmask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c) < 1000 and cv2.contourArea(c) > 5:
+            (x,y),radius = cv2.minEnclosingCircle(c)
+            center = (int(x),int(y))
+            radius = int(radius)
+            if radius<25:
+                if 0 in Track:
+                    for T in range(Max_Track_number):
+                        distance = np.sqrt((Track[T][-1][0]-x)**2 + (Track[T][-1][1]-y)**2)
+                        if distance < Min[T][0]:
+                            Min[T][1] = [x,y]
+                            Min[T][0] = distance
+                # cv2.circle(frame,center,(6),(0,0,255),2)
+                # cv2.circle(frame,center,(12),(0,255,0),2)
+
+    if 0 in Track:
+        for T in range(Max_Track_number):
+            if Min[T][0] < 20:
+                Track[T].append(Min[T][1])
+                Time[T].append(count)
+            Min[T][0] = np.inf
+            Min[T][1] = []
+            for i in range(len(Track[T])-1):
+                A = Track[T][i]
+                B = Track[T][i+1]
+                cv2.line(frame,(int(A[0]),int(A[1])),(int(B[0]),int(B[1])),(colors[T][0]*255,colors[T][1]*255,colors[T][2]*255),2)
+            # X = []
+            # Y = []
+            # for i in range(len(Track[T])):
+            #     X.append(Track[T][i][0])
+            #     Y.append(Track[T][i][1])
+            # if len(X)>4:
+            #     polynomial_features = PolynomialFeatures(degree=10,
+            #                                  include_bias=False)
+            #     linear_regression = LinearRegression()
+            #     pipeline = Pipeline([("polynomial_features", polynomial_features),
+            #                          ("linear_regression", linear_regression)])
+            #     time = np.sort(Time[T])
+            #     pipeline.fit(time[:, np.newaxis], X)
+            #     X = pipeline.predict(time[:, np.newaxis])
+            #     pipeline.fit(time[:, np.newaxis], Y)
+            #     Y = pipeline.predict(time[:, np.newaxis])
+            #     for i in range(len(X)-1):
+            #         t1 = Time[T][i]
+            #         t2 = Time[T][i+1]
+            #         cv2.line(frame,(int(X[i]),int(Y[i])),(int(X[i+1]) , int(Y[i+1])),(colors[T][0]*255,colors[T][1]*255,colors[T][2]*255),2)
+
+
+    cv2.imshow('video',frame)
+    if count<10:
+        frame_name = '000'+str(count)+'.jpg'
+    elif count<100:
+        frame_name = '00'+str(count)+'.jpg'
+    elif count<1000:
+        frame_name = '0'+str(count)+'.jpg'
+    cv2.imwrite('/home/omari/Bugs/images/2_frame_'+frame_name,frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
